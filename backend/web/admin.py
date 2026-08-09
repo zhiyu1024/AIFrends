69ec38f59572fab4709fd42e12666032a2961496
zhiyu1024 <fnex1024s@163.com>
2026-08-09 11:20:16 +0800

    定义数据库操作

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from web.models.user import UserProfile
 
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)
 # Register your models here.
