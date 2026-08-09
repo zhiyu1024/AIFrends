795d9498871a1d5babb8de55d763a2b5e96cb94a
zhiyu1024 <fnex1024s@163.com>
2026-08-09 10:42:50 +0800

    实现登录注册前端页面

--- frontend/src/views/user/account/RegisterIndex.vue
+++ frontend/src/views/user/account/RegisterIndex.vue
@@ -3,7 +3,27 @@
 </script>
 
 <template>
-注册
+  <div class="container flex justify-center mt-30">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+
+
+      <label class="label">Username</label>
+      <input type="text" class="input" placeholder="输入您的账号"/>
+
+      <label class="label">Password</label>
+      <input type="password" class="input" placeholder="请输入您的密码"/>
+
+      <label class="label">Confirm Password</label>
+      <input type="password" class="input" placeholder="请再次输入您的密码"/>
+
+      <router-link :to="{name :'user-login'}" class="btn btn-neutral mt-4">登录</router-link>
+      <div class="flex justify-end">
+        <router-link :to="{name :'user-register'}" class="btn btn-small">注册</router-link>
+      </div>
+    </fieldset>
+
+  </div>
+
 </template>
 
 <style scoped>
--- frontend/src/views/user/account/loginIndex.vue
+++ frontend/src/views/user/account/loginIndex.vue
@@ -3,7 +3,24 @@
 </script>
 
 <template>
-登录
+  <div class="container flex justify-center mt-30">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+
+
+      <label class="label">Username</label>
+      <input type="text" class="input" placeholder="账号"/>
+
+      <label class="label">Password</label>
+      <input type="password" class="input" placeholder="密码"/>
+
+      <button class="btn btn-neutral mt-4">登录</button>
+      <div class="flex justify-end">
+        <router-link :to="{name :'user-register'}" class="btn btn-small">注册</router-link>
+      </div>
+    </fieldset>
+
+  </div>
+
 </template>
 
 <style scoped>
