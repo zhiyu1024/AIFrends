49753298e0ad7a80369ed01ed4b268e26337a1e3
zhiyu1024 <fnex1024s@163.com>
2026-08-12 15:18:21 +0800

    创建守卫，实现某些页面非登录状态无法访问

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -7,6 +7,7 @@ import LoginIndex from "@/views/user/account/loginIndex.vue";
 import RegisterIndex from "@/views/user/account/RegisterIndex.vue";
 import ProfileIndex from "@/views/user/profile/ProfileIndex.vue";
 import SpaceIndex from "@/views/user/space/SpaceIndex.vue";
+import {useUserStore} from "@/stores/user.ts";
 
 
 const router = createRouter({
@@ -16,49 +17,89 @@ const router = createRouter({
       path:'/',
       component: HomepageIndex,
       name: 'homepage-index',
+      meta:{
+        needLogin: false,
+      }
     },
-      {
+    {
       path:'/friend',
       component: FriendIndex,
       name: 'friend-index',
+      meta:{
+        needLogin: true,
+      }
+
     },
-      {
+    {
       path:'/create/',
       component: CreateIndex,
       name: 'create-index',
+      meta:{
+        needLogin: true,
+      }
     },
-      {
+    {
       path:'/404',
       component: NotFoundIndex,
       name: '404',
+      meta:{
+        needLogin: false,
+      }
+
     },
-      {
+    {
       path:'/user/account/login',
       component: LoginIndex,
       name: 'user-login',
+      meta:{
+        needLogin: false,
+      }
     },
-      {
+    {
       path:'/user/account/register',
       component: RegisterIndex,
       name: 'user-register',
+      meta:{
+        needLogin: false,
+      }
     },
-      {
+    {
       path:'/user/profile',
       component: ProfileIndex,
       name: 'user-profile',
+      meta:{
+        needLogin: true,
+      }
     },
-      {
+    {
       path:'/user/space/:user_id/',
       component: SpaceIndex,
       name: 'user-space',
+      meta:{
+        needLogin: true,
+      }
     },
     {
       path:'/:pathMatch(.*)*',
       component: NotFoundIndex,
       name: 'not-found',
+      meta:{
+        needLogin: false,
+      }
     }
 
   ],
+})
+
+router.beforeEach((to, from) => {
+  const user = useUserStore()
+  if (to.meta.needLogin && !user.isLogin()) {
+    return {
+      name: 'user-login',
+    }
+  }else{
+    return true
+  }
 })
 
 export default router
